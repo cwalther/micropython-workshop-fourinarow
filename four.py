c6c7920fe620ced7d3ce2f1ef1a42ac1d34a23b2
Christian Walther <cwalther@gmx.ch>
2018-11-01 11:34:42 +0100

    3. Wrap code into a main() function.
    Advantages:
    - Can invoke main() again after it exits, without re-importing the module.
    - In MicroPython, local variables are more efficient than global ones.

--- four.py
+++ four.py
@@ -1,28 +1,32 @@
 import pew
 
-# -- initialization ----
+def main():
 
-pew.init()
-screen = pew.Pix()
-cursor = 3
+	# -- initialization ----
 
-# -- game loop ----
+	pew.init()
+	screen = pew.Pix()
+	cursor = 3
 
-while True:
+	# -- game loop ----
 
-	# -- input handling ----
+	while True:
 
-	k = pew.keys()
-	if k & pew.K_LEFT:
-		if cursor > 0:
-			cursor -= 1
-	if k & pew.K_RIGHT:
-		if cursor < 6:
-			cursor += 1
+		# -- input handling ----
 
-	# -- drawing ----
+		k = pew.keys()
+		if k & pew.K_LEFT:
+			if cursor > 0:
+				cursor -= 1
+		if k & pew.K_RIGHT:
+			if cursor < 6:
+				cursor += 1
 
-	screen.box(0, 0, 0, 7, 1)
-	screen.pixel(cursor, 0, 1)
-	pew.show(screen)
-	pew.tick(0.15)
+		# -- drawing ----
+
+		screen.box(0, 0, 0, 7, 1)
+		screen.pixel(cursor, 0, 1)
+		pew.show(screen)
+		pew.tick(0.15)
+
+main()
--- four_commented.py
+++ four_commented.py
@@ -1,40 +1,48 @@
 # plain import: import a whole module and make it available under its own name
 import pew
 
-# -- initialization ----
+# The main function contains the whole game. Called at the bottom.
+def main():
 
-pew.init()
-# the framebuffer
-screen = pew.Pix()
-# x coordinate of my cursor
-cursor = 3
+	# -- initialization ----
 
-# -- game loop ----
+	pew.init()
+	# the framebuffer
+	screen = pew.Pix()
+	# x coordinate of my cursor
+	cursor = 3
 
-while True:
+	# -- game loop ----
 
-	# -- input handling ----
+	while True:
 
-	k = pew.keys()
-	# check for bits in k using the bitwise AND operator - the result is zero or nonzero, which count as false or true
-	if k & pew.K_LEFT:
-		# move cursor left if possible
-		if cursor > 0:
-			cursor -= 1
-	if k & pew.K_RIGHT:
-		# move cursor right if possible
-		if cursor < 6:
-			cursor += 1
+		# -- input handling ----
 
-	# -- drawing ----
+		k = pew.keys()
+		# check for bits in k using the bitwise AND operator - the result is zero or nonzero, which count as false or true
+		if k & pew.K_LEFT:
+			# move cursor left if possible
+			if cursor > 0:
+				cursor -= 1
+		if k & pew.K_RIGHT:
+			# move cursor right if possible
+			if cursor < 6:
+				cursor += 1
 
-	# clear previous cursor
-	screen.box(0, 0, 0, 7, 1)
-	# draw cursor
-	screen.pixel(cursor, 0, 1)
-	# done drawing into the framebuffer, send it to the display
-	pew.show(screen)
-	# wait until it's time for the next frame
-	# 0.15 seconds is an appropriate frame time for our animations and key repeating - increase it if you still find it hard to move the cursor by exactly one pixel
-	# drawing at a faster rate would require more complex key repeat handling
-	pew.tick(0.15)
+		# -- drawing ----
+
+		# clear previous cursor
+		screen.box(0, 0, 0, 7, 1)
+		# draw cursor
+		screen.pixel(cursor, 0, 1)
+		# done drawing into the framebuffer, send it to the display
+		pew.show(screen)
+		# wait until it's time for the next frame
+		# 0.15 seconds is an appropriate frame time for our animations and key repeating - increase it if you still find it hard to move the cursor by exactly one pixel
+		# drawing at a faster rate would require more complex key repeat handling
+		pew.tick(0.15)
+
+	# end of game loop
+
+# run the game when this file is imported
+main()
