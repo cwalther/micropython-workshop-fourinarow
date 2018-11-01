48cbc1309d6aa1773fc689abc2e585e64b363d50
Christian Walther <cwalther@gmx.ch>
2018-11-01 11:22:30 +0100

    2. Moving cursor.

--- four.py
+++ four.py
@@ -4,6 +4,7 @@ import pew
 
 pew.init()
 screen = pew.Pix()
+cursor = 3
 
 # -- game loop ----
 
@@ -12,10 +13,16 @@ while True:
 	# -- input handling ----
 
 	k = pew.keys()
-	# modify state
+	if k & pew.K_LEFT:
+		if cursor > 0:
+			cursor -= 1
+	if k & pew.K_RIGHT:
+		if cursor < 6:
+			cursor += 1
 
 	# -- drawing ----
 
-	# draw on screen
+	screen.box(0, 0, 0, 7, 1)
+	screen.pixel(cursor, 0, 1)
 	pew.show(screen)
 	pew.tick(0.15)
--- four_commented.py
+++ four_commented.py
@@ -6,6 +6,8 @@ import pew
 pew.init()
 # the framebuffer
 screen = pew.Pix()
+# x coordinate of my cursor
+cursor = 3
 
 # -- game loop ----
 
@@ -14,13 +16,25 @@ while True:
 	# -- input handling ----
 
 	k = pew.keys()
-	# modify state
+	# check for bits in k using the bitwise AND operator - the result is zero or nonzero, which count as false or true
+	if k & pew.K_LEFT:
+		# move cursor left if possible
+		if cursor > 0:
+			cursor -= 1
+	if k & pew.K_RIGHT:
+		# move cursor right if possible
+		if cursor < 6:
+			cursor += 1
 
 	# -- drawing ----
 
-	# draw on screen
-
-    # when done, send it to the display
+	# clear previous cursor
+	screen.box(0, 0, 0, 7, 1)
+	# draw cursor
+	screen.pixel(cursor, 0, 1)
+	# done drawing into the framebuffer, send it to the display
 	pew.show(screen)
 	# wait until it's time for the next frame
+	# 0.15 seconds is an appropriate frame time for our animations and key repeating - increase it if you still find it hard to move the cursor by exactly one pixel
+	# drawing at a faster rate would require more complex key repeat handling
 	pew.tick(0.15)
